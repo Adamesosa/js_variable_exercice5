27625fc0ed5f1beac9c3a47518c90cfb4f4e0171
Adame <Adame.haouari@gmail.com>
2020-12-29 09:57:42 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+let num1 = 1
+
+let num2 = 2
+
+console.log(num1+num2)
+
+console.log(num1*num2)
+
+console.log(num1-num2)
+
+console.log(num1%num2)
+
+
+let age= prompt('Quel est votre age ?')
+
+let prenom = prompt('quel est votre prenom ?')
+
+console.log(`${prenom} a ${age} ans cette année`)
+
+
+console.log(`${prenom} aura ${age} ans dans 3 ans`)
+
+
+// Exercice 1
+// let nombre1 = 10;
+// let nombre2 = 2;
+// let addition = `${nombre1} + ${nombre2} = ${nombre1 + nombre2}`;
+// console.log(addition);
+
+// console.log(`${nombre1} - ${nombre2} = ${nombre1 - nombre2}`);
+// console.log(`${nombre1} * ${nombre2} = ${nombre1 * nombre2}`);
+// console.log(`${nombre1} / ${nombre2} = ${nombre1 / nombre2}`);
+
+// Exercice 2 
+// let prenom = prompt('Ton prénom ?');
+// let age = parseInt(prompt('Ton âge ?'));
+// console.log(`${prenom} a ${age} ans cette année`);
+// console.log(typeof age);
+// console.log(`${prenom} a ${age + 3} ans dans 3 ans`);
+
